c32f419d8e032dc6b81798ee70b37f79a257db5b
kavipriya s <132042779+kaira038@users.noreply.github.com>
2024-06-30 12:10:56 -0700

    Add files via upload

---
+++ Task.variable.js
@@ -0,0 +1,34 @@
+                                    //Javascript .Info
+
+                 // Task 01- creating variable
+
+let name;
+let admin;
+
+// name="John";
+// admin="John";    instead....
+
+name="john";
+admin=name;
+
+// alert(admin);
+
+
+                 // Task 02-  naming the variable
+
+let ourPlanetName="Earth";
+
+// let currentVisitorName="Kavipriya";    instead...
+let currentUserName ="Kavipriya";
+
+console.log(ourPlanetName);
+console.log(currentUserName);   
+
+
+                  // Task 03- Uppercase Const
+
+const BIRTHDAY='21.08.2002';
+const AGE= 21;
+
+console.log(BIRTHDAY);
+console.log(AGE);
