6e64db567a9566cc8ed83ac7ae2d3d69cdab5f4d
kavipriya s <132042779+kaira038@users.noreply.github.com>
2024-06-30 12:34:34 -0700

    Add files via upload

---
+++ Task.Datatype.js
@@ -0,0 +1,10 @@
+let name;
+ 
+name="kavi";
+               
+alert(`hello ${1}`);        //hello 1     (o/p)
+alert(`hello ${"name"}`);   //hello name  (o/p)
+alert(`hello ${name}`);     //hello kavi  (o/p)
+
+
+
