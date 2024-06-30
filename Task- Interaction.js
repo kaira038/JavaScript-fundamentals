727bbfb00fdac3a828514ab14674de0762fded59
kavipriya s <132042779+kaira038@users.noreply.github.com>
2024-06-30 13:36:26 -0700

    Add files via upload

---
+++ Task- Interaction.js
@@ -0,0 +1,9 @@
+
+//  TASK - create a web page that asks for a name and outputs
+
+let name=prompt("what is your name");
+
+alert(`my name is ${name}`);
+
+
+//  we can also write this code inside the script tag used inthe body part of the HTML document.
